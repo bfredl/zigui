d783f69b9594238a6726b3f6819b9fcf37871cdf
Björn Linse <bjorn.linse@gmail.com>
2020-09-07 08:23:39 +0200

    not a good look sir

--- build.zig
+++ build.zig
@@ -1,11 +1,22 @@
 const Builder = @import("std").build.Builder;
+const mem = @import("std").mem;
 const builtin = @import("builtin");
+
+const print = @import("std").debug.print;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const windows = b.option(bool, "windows", "create windows build") orelse false;
 
     var exe = b.addExecutable("zigui", "src/main.zig");
+    var basepath = "libvterm/src/";
+    var array  =  {"vterm.c", "foo.c"};
+    for (array) |sf| {
+      //var s = mem.concat(all, byte, .{basepath, sf});
+      //exe.addCSourceFile(s, &[_][]const u8{"-std=c99"});
+     // print(sf);
+      
+    }
     exe.setBuildMode(mode);
 
     if (windows) {
@@ -15,6 +26,8 @@ pub fn build(b: *Builder) void {
             .abi = .gnu,
         });
     }
+
+    exe.addIncludeDir("libvterm/include");
 
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("glfw");
